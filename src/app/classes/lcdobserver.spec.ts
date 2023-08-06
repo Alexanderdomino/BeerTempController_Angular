61306e621a71658bfbb5a6b85f0a9ca2e85ca5b3
alexanderdomino <alexander.domino@gmail.com>
2023-08-06 21:39:28 +0200

    Created the LCDObserver class (Not implemented)

---
+++ src/app/classes/lcdobserver.spec.ts
@@ -0,0 +1,7 @@
+import { LCDObserver } from './lcdobserver';
+
+describe('LCDObserver', () => {
+  it('should create an instance', () => {
+    expect(new LCDObserver()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/lcdobserver.ts
@@ -0,0 +1,12 @@
+import {IObserver} from "../interfaces/iobserver";
+
+export class LCDObserver implements IObserver {
+
+  //Initialize the Display
+
+  //Method to update the Display when temperature or configuration changes
+  Update(): void {
+    throw new Error("Method not implemented.");
+  }
+}
+
