e7cddb48826c50f63c8703a8af076c8baea7f75c
alexanderdomino <alexander.domino@gmail.com>
2023-08-06 21:57:18 +0200

    Implemented the Sensor Subject - valid temperatures are 1 to 50 degrees celsius

---
+++ src/app/classes/temp-sensor.spec.ts
@@ -0,0 +1,7 @@
+import { TempSensor } from './temp-sensor';
+
+describe('TempSensor', () => {
+  it('should create an instance', () => {
+    expect(new TempSensor()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/temp-sensor.ts
@@ -0,0 +1,42 @@
+import {ISubject} from "../interfaces/isubject";
+import {IObserver} from "../interfaces/iobserver";
+
+export class TempSensor implements ISubject {
+
+  //Members
+  private currentTemp: number = 0;
+
+  //Initialize the Temperature Sensor
+  sensor = require('ds18b20-raspi');
+
+  //Methods to register and unregister observers
+  RegisterObserver(o: IObserver): void {
+    throw new Error("Method not implemented.");
+  }
+
+  RemoveObserver(o: IObserver): void {
+    throw new Error("Method not implemented.");
+  }
+
+  //Method to notify observers of change
+  NotifyObservers(): void {
+    throw new Error("Method not implemented.");
+  }
+
+  //Method to get the current temperature
+  GetTemperature(): number {
+    return this.currentTemp;
+  }
+
+  //Method to read the current temperature
+  ReadTemperature(): void {
+
+    let tmpTemp: number
+    tmpTemp = this.sensor.readSimpleC(1);
+
+    if (1 < tmpTemp && tmpTemp < 50) {
+      this.currentTemp = tmpTemp;
+    }
+
+  }
+}
